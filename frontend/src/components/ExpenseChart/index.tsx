6f296a8bc4e57ce5ba45d28ca4998e4f15bf36e2
Deyvid William <deyvidwms@gmail.com>
2022-09-02 18:57:55 -0300

    tirando elementos não utilizados

--- frontend/src/components/ExpenseChart/index.tsx
+++ frontend/src/components/ExpenseChart/index.tsx
@@ -1,5 +1,7 @@
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import React from 'react';
+import { PieChart, Pie, Cell } from 'recharts';
+
+// https://recharts.org/en-US/examples/PieChartWithCustomizedLabel
 
 import style from './ExpenseChart.module.scss';
 
