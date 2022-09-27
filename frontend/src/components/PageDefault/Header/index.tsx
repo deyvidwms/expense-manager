7ee6dde9cd0f1ce4514f8cd56a0a999825e01caf
Deyvid William <deyvidwms@gmail.com>
2022-09-27 06:22:24 -0300

    colocando icone e deixando logo responsiva

--- frontend/src/components/PageDefault/Header/index.tsx
+++ frontend/src/components/PageDefault/Header/index.tsx
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from './Menu';
 
 import style from './Header.module.scss';
 
-import { ReactComponent as Logo } from 'assets/images/logo.svg';
+import logoWeb from 'assets/images/logo.svg';
+import logoMobile from 'assets/images/logo-mobile.svg';
+
+// import { ReactComponent as Logo } from 'assets/images/logo.svg';
+// import { ReactComponent as LogoMobile } from 'assets/images/logo-mobile.svg';
 import { FaUserAlt } from "react-icons/fa";
 
 const Header = () => {
   const [aberto, setAberto] = useState(false);
+  const [logo, setLogo] = useState<string>(logoWeb);
+
+  useEffect(() => {
+    function setLogoHeader() {
+      if ( window.innerWidth <= 480 )
+        setLogo(logoMobile);
+      else
+        setLogo(logo);
+
+    } 
+    setLogoHeader();
+    window.addEventListener('resize', setLogoHeader);
+  }, []);
+
 
   return (
     <div className={style.header}>
       
       <div className={style.logo}>
-        <Logo />
+        <img src={logo} alt="Logo do projeto" />
       </div>
 
       <button 
