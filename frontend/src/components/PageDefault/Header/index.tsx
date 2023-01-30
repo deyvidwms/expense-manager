01e979a1f37253804b05278662e32ca842d72e97
Deyvid William <deyvidwms@gmail.com>
2023-01-30 17:56:16 -0300

    correção responsividade e menu

--- frontend/src/components/PageDefault/Header/index.tsx
+++ frontend/src/components/PageDefault/Header/index.tsx
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from './Menu';
 
 import style from './Header.module.scss';
 
 import { ReactComponent as Logo } from 'assets/images/logo.svg';
-import { FaUserAlt } from "react-icons/fa";
+import { FaBars, FaTimes, FaUserAlt } from "react-icons/fa";
 
 const Header = () => {
   const [aberto, setAberto] = useState(false);
+  const [menuMobile, setMenuMobile] = useState(false);
+
+  useEffect( () => {
+
+    const verifyWidthScreen = () => {
+      const width = window.innerWidth;
+
+      if ( width <= 500 ) {
+        setMenuMobile(true);
+      } else {
+        setMenuMobile(false);
+      }
+
+    }
+
+    verifyWidthScreen();
+
+    window.addEventListener('resize', verifyWidthScreen);
+
+  }, []);
 
   return (
     <div className={style.header}>
@@ -16,14 +36,31 @@ const Header = () => {
         <Logo />
       </div>
 
-      <button 
-        className={style.userOptions} 
-        onClick={() => setAberto(!aberto)}
-        // onBlur={() => setAberto(false)}
-      >
-        <FaUserAlt />
-        <p>ADMIN</p>
-      </button>
+      {
+        !menuMobile ? (
+          <button 
+            className={style.userOptions} 
+            onClick={() => setAberto(!aberto)}
+            // onBlur={() => setAberto(false)}
+          >
+            <FaUserAlt />
+            <p>ADMIN</p>
+          </button>
+        ) : (
+          <button 
+            className={style.userOptions} 
+            onClick={() => setAberto(!aberto)}
+            // onBlur={() => setAberto(false)}
+          >
+            {
+              !aberto ?
+                <FaBars /> :
+                <FaTimes />
+            }
+          </button>
+        )
+      }
+
 
       <Menu aberto={aberto} setAberto={setAberto} />
     </div>
