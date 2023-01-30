29af9a775d989962e2a6037067e18a278bdce03f
Deyvid William <deyvidwms@gmail.com>
2023-01-30 19:25:13 -0300

    editar e excluir categoria

--- frontend/src/pages/Entradas/Categorias/index.tsx
+++ frontend/src/pages/Entradas/Categorias/index.tsx
@@ -11,6 +11,7 @@ const CategoriasEntradas = () => {
   const categoriesList = localStorage.getItem('categoriasEntrada') || null;
   const [categorias, setCategorias] = useState<{ id: string, name: string, created_at: string }[]>(categoriesList !== null ? JSON.parse(categoriesList) : []);
   const [categoria, setCategoria] = useState<string>('');
+  const [categoriaId, setCategoriaId] = useState<string>('');
 
   const handleChange = (value: string) => {
     setCategoria(value);
@@ -20,9 +21,11 @@ const CategoriasEntradas = () => {
     if (categoria.length > 0) {
       const localCategories = localStorage.getItem('categoriasEntrada') || null;
 
+      const index = categoriaId.length > 0 ? categorias.findIndex(element => element.id === categoriaId) : -1;
+
       const currentDate = new Date();
-      const createdAt = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}-${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getMilliseconds()}`;
-      const id = `${crypto.randomUUID()}-${createdAt}`;
+      const createdAt = index !== -1 ? categorias[index].created_at : `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}-${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getMilliseconds()}`;
+      const id = index !== -1 ? categorias[index].id : `${crypto.randomUUID()}-${createdAt}`;
 
       const category = {
         id: id,
@@ -31,10 +34,46 @@ const CategoriasEntradas = () => {
       }
 
       const tempCategories = localCategories !== null ? JSON.parse(localCategories) : [];
-      tempCategories.push(category);
+      if (index !== -1) {
+        tempCategories[index].name = category.name;
+      } else {
+        tempCategories.push(category);
+      }
       localStorage.setItem('categoriasEntrada', JSON.stringify(tempCategories));
       setCategorias(tempCategories);
+
+      setCategoria('');
+      setCategoriaId('');
     }
+  }
+
+  const handleClickDelete = (id: string) => {
+
+    const localCategories = localStorage.getItem('categoriasEntrada') || null;
+
+    if (localCategories !== null) {
+
+      const tempCategories = JSON.parse(localCategories);
+
+      const index = tempCategories.findIndex( (element: { id: string, name: string, created_at: string }) => element.id === id );
+      tempCategories.splice(index, 1);
+      
+      localStorage.setItem('categoriasEntrada', JSON.stringify(tempCategories));
+      setCategorias(tempCategories)
+
+      setCategoria('');
+      setCategoriaId('');
+    }
+
+  }
+
+  const handleClickToEdit = (category: {
+    id: string,
+    name: string,
+    created_at: string
+  }) => {
+    setCategoriaId(category.id);
+    setCategoria(category.name);
   }
 
   return (
@@ -83,43 +122,43 @@ const CategoriasEntradas = () => {
               <tbody>
                 {
                   categorias.length > 0 ?
-                  categorias.map(element => {
-                    const months = [
-                      { name: 'Janeiro', sig: 'Jan' },
-                      { name: 'Fevereiro', sig: 'Fev' },
-                      { name: 'Março', sig: 'Mar' },
-                      { name: 'Abril', sig: 'Abr' },
-                      { name: 'Maio', sig: 'Mai' },
-                      { name: 'Junho', sig: 'Jun' },
-                      { name: 'Julho', sig: 'Jul' },
-                      { name: 'Agosto', sig: 'Ago' },
-                      { name: 'Setembro', sig: 'Set' },
-                      { name: 'Outubro', sig: 'Out' },
-                      { name: 'Novembro', sig: 'Nov' },
-                      { name: 'Dezembro', sig: 'Dez' }
-                    ];
+                    categorias.map(element => {
+                      const months = [
+                        { name: 'Janeiro', sig: 'Jan' },
+                        { name: 'Fevereiro', sig: 'Fev' },
+                        { name: 'Março', sig: 'Mar' },
+                        { name: 'Abril', sig: 'Abr' },
+                        { name: 'Maio', sig: 'Mai' },
+                        { name: 'Junho', sig: 'Jun' },
+                        { name: 'Julho', sig: 'Jul' },
+                        { name: 'Agosto', sig: 'Ago' },
+                        { name: 'Setembro', sig: 'Set' },
+                        { name: 'Outubro', sig: 'Out' },
+                        { name: 'Novembro', sig: 'Nov' },
+                        { name: 'Dezembro', sig: 'Dez' }
+                      ];
 
-                    const createdAtSlipted = element.created_at.split("-");
+                      const createdAtSlipted = element.created_at.split("-");
 
-                    const createdAt = `${createdAtSlipted[2].length === 1 ?
-                      `0${createdAtSlipted[2]}` :
-                      createdAtSlipted[2]
-                      } de ${months[(Number(createdAtSlipted[1]) - 1)].name.toLocaleLowerCase()} de ${createdAtSlipted[0]}`;
+                      const createdAt = `${createdAtSlipted[2].length === 1 ?
+                        `0${createdAtSlipted[2]}` :
+                        createdAtSlipted[2]
+                        } de ${months[(Number(createdAtSlipted[1]) - 1)].name.toLocaleLowerCase()} de ${createdAtSlipted[0]}`;
 
-                    return (
-                      <tr key={element.id}>
-                        <td> {element.name} </td>
-                        <td> {createdAt} </td>
-                        <td> <FaPencilAlt /> </td>
-                        <td> <FaTrashAlt /> </td>
+                      return (
+                        <tr key={element.id}>
+                          <td> {element.name} </td>
+                          <td> {createdAt} </td>
+                          <td style={{ cursor: 'pointer' }} onClick={() => handleClickToEdit(element)}> <FaPencilAlt /> </td>
+                          <td style={{ cursor: 'pointer' }} onClick={() => handleClickDelete(element.id)}> <FaTrashAlt /> </td>
+                        </tr>
+                      )
+                    }) :
+                    (
+                      <tr>
+                        <td colSpan={4} style={{ textAlign: 'center' }}>Nenhuma informação encontrada</td>
                       </tr>
                     )
-                  }) : 
-                  (
-                    <tr>
-                      <td colSpan={4} style={{textAlign: 'center'}}>Nenhuma informação encontrada</td>
-                    </tr>
-                  )
                 }
               </tbody>
 
